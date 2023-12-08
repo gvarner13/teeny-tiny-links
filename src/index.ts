cda56e820ab7bb0ddd2e676e0bc240bccb391ef9
Gary Varner <garysarahvarner@gmail.com>
2023-12-07 22:20:06 -0500

    added test redirect and 404 message

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
+app.notFound((c) => {
+  return c.text("Your looking in the wrong spot", 404);
+});
+
 app.get("/", (c) => c.text("Welcome to my worker!"));
+
+app.get("/redirect", (c) => {
+  return c.redirect("https://github.com/gvarner13");
+});
 
 export default app;
