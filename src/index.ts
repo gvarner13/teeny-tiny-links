3bb1a31cc41e5301d619e1144e0499fd4399adbf
Gary Varner <garysarahvarner@gmail.com>
2023-12-09 11:07:22 -0500

    adding zod for validation and new links route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 const app = new Hono();
 
@@ -11,5 +13,21 @@ app.get("/", (c) => c.text("Welcome to my worker!"));
 app.get("/redirect", (c) => {
   return c.redirect("https://github.com/gvarner13");
 });
+
+app.post(
+  "/links",
+  zValidator(
+    "json",
+    z.object({
+      url: z.string().url(),
+    })
+  ),
+  (c) => {
+    const { url } = c.req.valid("json");
+    return c.jsonT({
+      message: `${url} has been verified`,
+    });
+  }
+);
 
 export default app;
